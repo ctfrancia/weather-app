c81f44238f5899c476d4f54b6afcfd8936a85eb8
Christian Francia <CTFrancia@gmail.com>
2019-02-20 11:33:36 +0100

    chore: add api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,23 +3,28 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
+import constants from '../_private/constants';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private endPoint: string = 'api.openweathermap.org/data/2.5/weather?q=';
+  private httpOptions: object = new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
 
-  constructor(private http: HttpClient) {
-    const endPoint = 'api.openweathermap.org/data/2.5/weather?q=';
-    const httpOptions = new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }
+  constructor(private http: HttpClient) {}
+
   private extractData(res: Response) {
     let body = res;
-    return body || {};
+    console.log('BODY: ', body);
 
+    return body || {};
   }
 
-
-
+  getWeather(city: string): Observable<any> {
+    return this.http.get(`${this.endPoint} ${constants.apiKey}` + city)
+    .pipe(map(this.extractData));
+  }
 };
